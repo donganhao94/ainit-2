adefb6f586036e28d0bdf10b82f83bcc3c700457
do ngan hao <donganhao94@gmail.com>
2023-09-22 21:05:06 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grid, StyledEngineProvider, ThemeProvider } from "@mui/material";
+import DetailMessage from "page/DetailMessage";
+import React, { useCallback, useReducer } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { appContext, AppData, appData } from "./context";
+import Noti from "./page/Noti";
+import UserInfo from "./page/UserInfo";
+import { theme } from "./theme";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      suspense: false,
+      useErrorBoundary: false,
+      cacheTime: 1000 * 60 * 60 * 24,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
+  const [state, dispatch] = useReducer((state: any, action: any) => {
+    switch (action.type) {
+      case "SET_STATE":
+        return {
+          ...state,
+          ...action.payload,
+        };
+      default:
+        return { ...state };
+    }
+  }, appData);
+  const setState = useCallback((state: Omit<AppData, "setState">) => {
+    dispatch({
+      type: "SET_STATE",
+      payload: state,
+    });
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <QueryClientProvider client={queryClient}>
+          <appContext.Provider value={{ ...state, setState }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6} lg={4}>
+                <UserInfo />
+              </Grid>
+              <Grid item xs={12} md={6} lg={4}>
+                <Noti />
+              </Grid>
+              <Grid item xs={12} md={6} lg={4}>
+                <DetailMessage />
+              </Grid>
+            </Grid>
+          </appContext.Provider>
+        </QueryClientProvider>
+      </ThemeProvider>
+    </StyledEngineProvider>
   );
 }
 
